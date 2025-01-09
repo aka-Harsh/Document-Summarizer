bca0aca95025750164b5857c81b5ea41d6713e8d
Harsh Mehta <98632476+aka-Harsh@users.noreply.github.com>
2025-01-09 15:49:19 +0530

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into product_details(movie_title,movie_language,movie_release_date,movie_image_url)
+values ('American Pie','English','2024-08-09','');
+
+insert into product_details(movie_title,movie_language,movie_release_date,movie_image_url) 
+values ('Rush Hour','English','2024-08-10','');
