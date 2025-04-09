f2c02ab2dfaabd02466502a0b8bf94a6959d33c8
Harsh Mehta <98632476+aka-Harsh@users.noreply.github.com>
2025-04-09 11:35:58 +0530

    Delete src directory

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into movie_details(movie_title,movie_language,movie_release_date,movie_image_url)
-values ('American Pie','English','2024-08-09','');
-
-insert into movie_details(movie_title,movie_language,movie_release_date,movie_image_url) 
-values ('Rush Hour','English','2024-08-10','');
